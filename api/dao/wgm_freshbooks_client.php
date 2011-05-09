ba218399c061c5bdbd43631fa22de3a80aa513c7
Jeff Standen <jeff@webgroupmedia.com>
2011-05-08 17:00:07 -0700

    5.4 fixes

--- api/dao/wgm_freshbooks_client.php
+++ api/dao/wgm_freshbooks_client.php
@@ -132,7 +132,7 @@ class DAO_WgmFreshbooksClient extends C4_ORMHelper {
 		$fields = SearchFields_WgmFreshbooksClient::getFields();
 		
 		// Sanitize
-		if(!isset($fields[$sortBy]))
+		if('*'==substr($sortBy,0,1) || !isset($fields[$sortBy]))
 			$sortBy=null;
 
         list($tables,$wheres) = parent::_parseSearchParams($params, $columns, $fields, $sortBy);
