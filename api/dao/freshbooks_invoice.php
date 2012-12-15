ab7c2574e92ef7c36fae74687916e75c9ae3c5a8
Jeff Standen <jeff@webgroupmedia.com>
2012-12-15 00:46:30 -0800

    6.2

--- api/dao/freshbooks_invoice.php
+++ api/dao/freshbooks_invoice.php
@@ -22,10 +22,42 @@ class DAO_FreshbooksInvoice extends C4_ORMHelper {
 	}
 
 	static function update($ids, $fields) {
-		parent::_update($ids, 'freshbooks_invoice', $fields);
+		if(!is_array($ids))
+			$ids = array($ids);
+		
+		// Make a diff for the requested objects in batches
+		
+		$chunks = array_chunk($ids, 100, true);
+		while($batch_ids = array_shift($chunks)) {
+			if(empty($batch_ids))
+				continue;
+			
+			// Get state before changes
+			$object_changes = parent::_getUpdateDeltas($batch_ids, $fields, get_class());
 
-		// Log the context update
-		//DevblocksPlatform::markContextChanged('example.context', $ids);
+			// Make changes
+			parent::_update($batch_ids, 'freshbooks_invoice', $fields);
+			
+			// Send events
+			if(!empty($object_changes)) {
+				// Local events
+				//self::_processUpdateEvents($object_changes);
+				
+				// Trigger an event about the changes
+				$eventMgr = DevblocksPlatform::getEventService();
+				$eventMgr->trigger(
+					new Model_DevblocksEvent(
+						'dao.freshbooks_invoice.update',
+						array(
+							'objects' => $object_changes,
+						)
+					)
+				);
+				
+				// Log the context update
+				//DevblocksPlatform::markContextChanged('cerberusweb.contexts.freshbooks.invoice', $batch_ids);
+			}
+		}
 	}
 
 	static function updateWhere($fields, $where) {
