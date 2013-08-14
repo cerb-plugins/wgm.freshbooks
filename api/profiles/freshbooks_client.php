44db978d5b2bdb8f0026779edbddf6fc104a7b9f
Jeff Standen <jeff@webgroupmedia.com>
2013-08-13 18:01:44 -0700

    6.5

--- api/profiles/freshbooks_client.php
+++ api/profiles/freshbooks_client.php
@@ -78,7 +78,12 @@ class PageSection_ProfilesFreshbooksClient extends Extension_PageSection {
 		$tpl->assign('properties', $properties);
 		
 		// Macros
-// 		$macros = DAO_TriggerEvent::getByOwner(CerberusContexts::CONTEXT_WORKER, $active_worker->id, 'event.macro.crm.opportunity');
+//		$macros = DAO_TriggerEvent::getByVirtualAttendantOwners(
+//			array(
+//				array(CerberusContexts::CONTEXT_WORKER, $active_worker->id),
+//			),
+//			'event.macro.'
+//		);
 // 		$tpl->assign('macros', $macros);
 		
 		$tpl->display('devblocks:wgm.freshbooks::profile.tpl');
